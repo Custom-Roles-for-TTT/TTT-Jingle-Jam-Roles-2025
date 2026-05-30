b985dba53984eef11727455559a4f95afd3cc18d
Malivil <mlebson@outlook.com>
2026-05-30 18:31:45 -0400

    Added start of shop logic

--- lua/customroles/gamer/gamer.lua
+++ lua/customroles/gamer/gamer.lua
@@ -1,1 +1,57 @@
--- TODO
+local hook = hook
+local math = math
+local net = net
+local table = table
+local util = util
+
+local AddHook = hook.Add
+local MathRandom = math.random
+local TableInsert = table.insert
+
+util.AddNetworkString("TTTGamerGachaStart")
+
+local function ChooseRandomPrize(ply)
+    local chance = MathRandom()
+    local targetRarity = GAMER.Rarities.Common
+    if chance <= GAMER.Config.Rarities[GAMER.Rarities.Legendary].Chance then
+        targetRarity = GAMER.Rarities.Legendary
+    elseif chance <= GAMER.Config.Rarities[GAMER.Rarities.Epic].Chance then
+        targetRarity = GAMER.Rarities.Epic
+    elseif chance <= GAMER.Config.Rarities[GAMER.Rarities.Rare].Chance then
+        targetRarity = GAMER.Rarities.Rare
+    elseif chance <= GAMER.Config.Rarities[GAMER.Rarities.Uncommon].Chance then
+        targetRarity = GAMER.Rarities.Uncommon
+    end
+
+    local prizes = {}
+    for _, prize in pairs(GAMER.Prizes) do
+        -- TODO: Check if this player already has a unique prize. If so, skip other unique prizes
+        -- TODO: What happens if a player gets a duplicate?
+        if prize.Rarity == targetRarity then
+            TableInsert(prizes, prize)
+        end
+    end
+
+    -- TODO: What if prizes is empty somehow?
+
+    return prizes[MathRandom(#prizes)]
+end
+
+AddHook("TTTOrderedEquipment", function(ply, id, isequip)
+    if id == EQUIP_GAMER_DORITOS then
+        -- TODO: Gain 2 gacha rolls
+    elseif id == EQUIP_GAMER_MTDEW then
+        -- TODO: 20% speed increase and triple jump
+    elseif id == EQUIP_GAMER_CHEETOS then
+        -- TODO: Heals you to full and allows you to smear your dirty fingers on someone to track them through the round
+    elseif id == EQUIP_GAMER_SPAGHETTI then
+        -- TODO: Provides 5% health regen per second (unsure if permanent or not)
+    elseif id == EQUIP_GAMER_MILK then
+        -- TODO: ??
+    elseif id == EQUIP_GAMER_GACHA then
+        local prize = ChooseRandomPrize()
+        net.Start("TTTGamerGachaStart")
+            net.WriteString(prize.Id)
+        net.Send(ply)
+    end
+end)
--- lua/customroles/gamer/sh_gamer.lua
+++ lua/customroles/gamer/sh_gamer.lua
@@ -4,6 +4,7 @@ local table = table
 
 local FileFind = file.Find
 local TableHasValue = table.HasValue
+local TableInsert = table.insert
 
 local ROLE = {}
 
@@ -31,6 +32,103 @@ ROLE.translations = {
     }
 }
 
+EQUIP_GAMER_DORITOS   = EQUIP_GAMER_DORITOS   or GenerateNewEquipmentID()
+EQUIP_GAMER_MTDEW     = EQUIP_GAMER_MTDEW     or GenerateNewEquipmentID()
+EQUIP_GAMER_CHEETOS   = EQUIP_GAMER_CHEETOS   or GenerateNewEquipmentID()
+EQUIP_GAMER_SPAGHETTI = EQUIP_GAMER_SPAGHETTI or GenerateNewEquipmentID()
+EQUIP_GAMER_MILK      = EQUIP_GAMER_MILK      or GenerateNewEquipmentID()
+EQUIP_GAMER_GACHA     = EQUIP_GAMER_GACHA     or GenerateNewEquipmentID()
+
+local function InitializeEquipment()
+    if DefaultEquipment then
+        DefaultEquipment[ROLE_GAMER] = {
+            EQUIP_GAMER_DORITOS,
+            EQUIP_GAMER_MTDEW,
+            EQUIP_GAMER_CHEETOS,
+            EQUIP_GAMER_SPAGHETTI,
+            EQUIP_GAMER_MILK,
+            EQUIP_GAMER_GACHA
+        }
+    end
+
+    if EquipmentItems then
+        if not EquipmentItems[ROLE_GAMER] then
+            EquipmentItems[ROLE_GAMER] = {}
+        end
+
+        -- If we haven't already registered this item, add it to the list
+        if not table.HasItemWithPropertyValue(EquipmentItems[ROLE_GAMER], "id", EQUIP_GAMER_DORITOS) then
+            TableInsert(EquipmentItems[ROLE_GAMER], {
+                id = EQUIP_GAMER_DORITOS,
+                type = "item_passive",
+                material = "vgui/ttt/gamer/icon_doritos",
+                name = "item_gamer_doritos",
+                desc = "item_gamer_doritos_desc",
+                norandom = true
+            })
+        end
+
+        if not table.HasItemWithPropertyValue(EquipmentItems[ROLE_GAMER], "id", EQUIP_GAMER_MTDEW) then
+            TableInsert(EquipmentItems[ROLE_GAMER], {
+                id = EQUIP_GAMER_MTDEW,
+                type = "item_passive",
+                material = "vgui/ttt/gamer/icon_mtdew",
+                name = "item_gamer_mtdew",
+                desc = "item_gamer_mtdew_desc",
+                norandom = true
+            })
+        end
+
+        if not table.HasItemWithPropertyValue(EquipmentItems[ROLE_GAMER], "id", EQUIP_GAMER_CHEETOS) then
+            TableInsert(EquipmentItems[ROLE_GAMER], {
+                id = EQUIP_GAMER_CHEETOS,
+                type = "item_passive",
+                material = "vgui/ttt/gamer/icon_cheetos",
+                name = "item_gamer_cheetos",
+                desc = "item_gamer_cheetos_desc",
+                norandom = true
+            })
+        end
+
+        if not table.HasItemWithPropertyValue(EquipmentItems[ROLE_GAMER], "id", EQUIP_GAMER_SPAGHETTI) then
+            TableInsert(EquipmentItems[ROLE_GAMER], {
+                id = EQUIP_GAMER_SPAGHETTI,
+                type = "item_passive",
+                material = "vgui/ttt/gamer/icon_spaghetti",
+                name = "item_gamer_spaghetti",
+                desc = "item_gamer_spaghetti_desc",
+                norandom = true
+            })
+        end
+
+        if not table.HasItemWithPropertyValue(EquipmentItems[ROLE_GAMER], "id", EQUIP_GAMER_MILK) then
+            TableInsert(EquipmentItems[ROLE_GAMER], {
+                id = EQUIP_GAMER_MILK,
+                type = "item_passive",
+                material = "vgui/ttt/gamer/icon_milk",
+                name = "item_gamer_milk",
+                desc = "item_gamer_milk_desc",
+                norandom = true
+            })
+        end
+
+        if not table.HasItemWithPropertyValue(EquipmentItems[ROLE_GAMER], "id", EQUIP_GAMER_GACHA) then
+            TableInsert(EquipmentItems[ROLE_GAMER], {
+                id = EQUIP_GAMER_GACHA,
+                type = "item_passive",
+                material = "vgui/ttt/gamer/icon_gacha",
+                name = "item_gamer_gacha",
+                desc = "item_gamer_gacha_desc",
+                norandom = true
+            })
+        end
+    end
+end
+InitializeEquipment()
+
+hook.Add("Initialize", "Gamer_Equipment_Initialize", InitializeEquipment)
+hook.Add("TTTPrepareRound", "Gamer_Equipment_Initialize", InitializeEquipment)
+
 RegisterRole(ROLE)
 
 GAMER = GAMER or {}
@@ -43,11 +141,11 @@ GAMER.Rarities = GAMER.Rarities or {
 }
 GAMER.Config = GAMER.Config or {
     Rarities = {
-        [GAMER.Rarities.Common] = { Name = "gamer_rarity_common", Color = Color(166, 166, 166) },
-        [GAMER.Rarities.Uncommon] = { Name = "gamer_rarity_uncommon", Color = Color(0, 182, 33) },
-        [GAMER.Rarities.Rare] = { Name = "gamer_rarity_rare", Color = Color(0, 146, 240) },
-        [GAMER.Rarities.Epic] = { Name = "gamer_rarity_epic", Color = Color(143, 21, 185) },
-        [GAMER.Rarities.Legendary] = { Name = "gamer_rarity_legendary", Color = Color(255, 192, 0) }
+        [GAMER.Rarities.Common] = { Name = "gamer_rarity_common", Color = Color(166, 166, 166), Chance = 0.50 },
+        [GAMER.Rarities.Uncommon] = { Name = "gamer_rarity_uncommon", Color = Color(0, 182, 33), Chance = 0.20 },
+        [GAMER.Rarities.Rare] = { Name = "gamer_rarity_rare", Color = Color(0, 146, 240), Chance = 0.15 },
+        [GAMER.Rarities.Epic] = { Name = "gamer_rarity_epic", Color = Color(143, 21, 185), Chance = 0.10 },
+        [GAMER.Rarities.Legendary] = { Name = "gamer_rarity_legendary", Color = Color(255, 192, 0), Chance = 0.05 }
     }
     -- TODO: Timing?
 }
